0c4156f98b36e3ffe599afba14ce4f4d7882b275
Thiago <thiagords.avd@gmail.com>
2021-09-08 23:55:49 -0300

    Aula08-09

---
+++ Ciclo4/Aula4/modulo4/src/pages/Servico/VisualizarServico/index.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Alert, Container, Table } from "reactstrap"
+
+import { api } from '../../../config';
+
+export const VisualizarServico = () => {
+
+    const [data, setData] = useState([]);
+
+    const [status, setStatus] = useState({
+        type: '',
+        message: ''
+    });
+
+    const getServicos = async () => {
+        await axios.get(api + "/listaservicos")
+            .then((response) => {
+                console.log(response.data.servicos);
+                setData(response.data.servicos);
+            })
+            .catch(() => {
+                setStatus({
+                    type: 'error',
+                    message: 'Erro:Não foi possível conectar a API.'
+                });
+            })
+    }
+
+    useEffect(() => {
+        getServicos();
+    }, []);
+
+    return (
+        <div className="p-3">
+            <Container>
+               {status.type === 'error' ? <Alert color="danger">{status.message}</Alert> :""}
+               <div className="d-flex">
+                    <div className="mr-auto">
+                        <h1>Informações do Serviço</h1>
+                    </div>
+                    <div className="p-2">
+                        <Link to="/cadastrarservico"
+                            className="btn btn-outline-primary btn-sm">Cadastrar
+                        </Link>
+                    </div>
+                </div>
+                <Table striped hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Serviço</th>
+                            <th>Descrição</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(item => (
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.nome}</td>
+                                <td>{item.descricao}</td>
+                                <td className="text-center">
+                                    <Link to={"/servico/"+ item.id}
+                                    className="btn btn-outline-primary btn-sm">Consultar</Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Container>
+        </div>
+    )
+}
